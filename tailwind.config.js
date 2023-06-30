cd8f0a520e05b4319824dc0cfbe9f717e778edc0
Aditio <faditiokur@gmail.com>
2023-06-30 22:29:48 +0700

    Update color and logo

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,8 @@ module.exports = {
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+        './app/Http/Livewire/*.php',
+        './vendor/wire-elements/modal/src/ModalComponent.php',
     ],
 
     theme: {
