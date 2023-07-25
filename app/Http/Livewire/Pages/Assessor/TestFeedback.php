b15bc15ce318d3529a0ae19aacc217c1d3de8f43
Aditio <faditiokur@gmail.com>
2023-07-25 12:17:27 +0700

    Update test feedback for assessor

--- app/Http/Livewire/Pages/Assessor/TestFeedback.php
+++ app/Http/Livewire/Pages/Assessor/TestFeedback.php
@@ -2,12 +2,55 @@
 
 namespace App\Http\Livewire\Pages\Assessor;
 
+use App\Models\TestSchedule;
+use App\Models\FeedbackComponent;
+use App\Models\TestFeedback as TestFeedbackModel;
+use Carbon\Carbon;
 use Livewire\Component;
 
 class TestFeedback extends Component
 {
+    public TestSchedule $testSchedule;
+    public $testFeedback;
+
+    protected $validationAttributes = [
+        'testFeedback.*'        => 'Umpan Balik',
+        'testFeedback.*.result' => 'Hasil',
+        'testFeedback.*.note'   => 'Catatan atau Komentar',
+    ];
+
+    public function mount()
+    {
+        $feedbackComponents = FeedbackComponent::all();
+
+        foreach ($feedbackComponents as $component) {
+            $testFeedback = TestFeedbackModel::where('feedback_component_id', $component->id)->first();
+
+            $this->testFeedback[$component->id]['result'] = null;
+            $this->testFeedback[$component->id]['note']   = null;
+
+            if (!empty($testFeedback)) {
+                $this->testFeedback[$component->id]['result'] = $testFeedback->result;
+                $this->testFeedback[$component->id]['note']   = $testFeedback->note;
+            }
+        }
+    }
+
+    public function back()
+    {
+        return redirect()->route('assessor.test.observation', ['testSchedule' => $this->testSchedule->id]);
+    }
+
+    public function next()
+    {
+        return redirect()->route('assessor.test.report', ['testSchedule' => $this->testSchedule->id]);
+    }
+
+    
     public function render()
     {
-        return view('livewire.pages.assessor.test-feedback');
+        $feedbackComponents = FeedbackComponent::all();
+
+        return view('livewire.pages.assessor.test-feedback', compact('feedbackComponents'));
     }
 }
--- resources/views/livewire/pages/assessor/test-feedback.blade.php
+++ resources/views/livewire/pages/assessor/test-feedback.blade.php
@@ -1,3 +1,122 @@
 <div>
-    {{-- If you look to others for fulfillment, you will never truly be fulfilled. --}}
+    <div>
+        <header class="bg-white shadow">
+            <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                    {{ __('Umpan Balik Asesmen') }}
+                </h2>
+                <p>{{ __('Pengisian umpan balik asesmen untuk saran dan perbaikan selanjutnya.') }}</p>
+            </div>
+        </header>
+
+        <div class="py-8">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg my-2 text-sm">
+
+                    <div id="form-wizard" class="p-8">
+                        @if (!empty($testSchedule->assessor_submitted_test_practice_at) &&
+                        !empty($testSchedule->participant_responded_test_practice_at) &&
+                        !empty($testSchedule->participant_responded_feedback_at))
+                        <div class="flex items-center p-4 mb-4 text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400"
+                            role="alert">
+                            <span class="mr-3">✅</span>
+                            <span class="sr-only"></span>
+                            <div>
+                                <span class="font-bold">{{ __('Form Umpan Balik Selesai') }}</span> {{
+                                __('Form umpan balik selesai diisikan. Silakan menunggu hasil kompetensi di halaman
+                                Hasil Kompetensi') }}
+                            </div>
+                        </div>
+                        @endif
+
+                        <table class="table-auto border-collapse border border-slate-400 w-1/2 text-left mt-3">
+                            <tbody>
+                                <tr>
+                                    <th class="border border-slate-300 w-2/5 p-2">{{ __('Nama Asesi') }}</th>
+                                    <td class="border border-slate-300 w-3/5 p-2">{{ $testSchedule->participant->name }}
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <th class="border border-slate-300 w-2/5 p-2">{{ __('Nama Asesor') }}</th>
+                                    <td class="border border-slate-300 w-3/5 p-2">{{ $testSchedule->assessor->name }}
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <th class="border border-slate-300 w-2/5 p-2">{{ __('Tanggal Ujian') }}</th>
+                                    <td class="border border-slate-300 w-3/5 p-2">{{
+                                        \Carbon\Carbon::parse($testSchedule->scheduled_at)->translatedFormat('l, j F Y')
+                                        }}
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+
+                        <hr class="my-5">
+
+                        <div class="w-full mt-5">
+                            <table
+                                class="table-auto border-collapse border border-slate-300 w-full text-left text-slate-900 mt-3">
+                                <thead>
+                                    <tr>
+                                        <th class="border border-slate-300 w-2/5 p-3 text-slate-900" rowspan="2">
+                                            Komponen</th>
+                                        <th class="border border-slate-300 w-1/5 p-3 text-slate-900 text-center"
+                                            colspan="2">Hasil</th>
+                                        <th class="border border-slate-300 w-2/5 p-3 text-slate-900 text-center"
+                                            rowspan="2">Catatan /
+                                            Komentar Asesi</th>
+                                    </tr>
+                                    <tr>
+                                        <th class="border border-slate-300 p-3 text-slate-900 text-center">Ya</th>
+                                        <th class="border border-slate-300 p-3 text-slate-900 text-center">Tidak</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($feedbackComponents as $component)
+                                    <tr>
+                                        <td class="border p-3">{{ $component->component }}</td>
+                                        <td class="border p-3 text-center">
+                                            @if (empty($testFeedback[$component->id]['result']))
+                                            -
+                                            @else
+                                            {{ $testFeedback[$component->id]['result'] === 'Y' ? '✅' : '' }}
+                                            @endif
+                                        </td>
+                                        <td class="border p-3 text-center ">
+                                            @if (empty($testFeedback[$component->id]['result']))
+                                            -
+                                            @else
+                                            {{ $testFeedback[$component->id]['result'] === 'T' ? '✅' : '' }}
+                                            @endif
+                                        </td>
+                                        <td class="border p-3">
+                                            @php $testFeedbackNote = 'testFeedback.' . $component->id . '.note' @endphp
+
+                                            @if (empty($testFeedback[$component->id]['note']))
+                                            -
+                                            @else
+                                            <p>{{ $testFeedback[$component->id]['note'] }}</p>
+                                            @endif
+                                        </td>
+                                    </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+
+                        <hr class="my-5">
+
+                        <div class="flex justify-between">
+                            <x-secondary-button wire:click="back()">{{ __('Kembali ke Tugas Observasi') }}
+                            </x-secondary-button>
+
+                            <x-primary-button wire:click="submit()">{{ __('Lanjut Laporan Kompetensi') }}
+                            </x-primary-button>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
