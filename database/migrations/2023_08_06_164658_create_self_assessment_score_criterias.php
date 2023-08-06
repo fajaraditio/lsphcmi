dc9acb3c63f4b881aff038033248f3a0fbdf9215
Aditio <faditiokur@gmail.com>
2023-08-07 00:31:07 +0700

    Adding scoring migration

---
+++ database/migrations/2023_08_06_164658_create_self_assessment_score_criterias.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('self_assessment_score_criterias', function (Blueprint $table) {
+            $table->id();
+            $table->text('title')->nullable();
+            $table->integer('score')->default(1);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('self_assessment_score_criterias');
+    }
+};
