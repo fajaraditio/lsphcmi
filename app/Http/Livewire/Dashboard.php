38fa591eb9ce428b444b16dc3f91f955605ef586
Aditio <faditiokur@gmail.com>
2023-08-16 11:45:27 +0700

    Adding dashboard card

--- app/Http/Livewire/Dashboard.php
+++ app/Http/Livewire/Dashboard.php
@@ -2,17 +2,28 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\TestSchedule;
+use App\Models\Participant;
+use App\Models\User;
 use Livewire\Component;
 
 class Dashboard extends Component
 {
     public $role;
+    public $participantCount;
+    public $assessorCount;
+    public $scheduledParticipantCount;
+    public $certifiedParticipantCount;
 
     public function mount()
     {
-        $this->role = auth()->user()->role->slug;
+        $this->role             = auth()->user()->role->slug;
+        $this->participantCount = Participant::count();
+        $this->assessorCount    = User::whereHas('role', fn ($model) => $model->where('slug', 'assessor'))->count();
+        $this->scheduledParticipantCount = TestSchedule::whereNotNull('scheduled_at')->count();
+        $this->certifiedParticipantCount = TestSchedule::whereNotNull('chief_approved_report_at')->count();
     }
-    
+
     public function render()
     {
         return view('livewire.dashboard');
--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -11,6 +11,28 @@
                 <div class="p-6 text-sm text-gray-900">
                     <span>Halo, {{ auth()->user()->name }} 👋</span>
                 </div>
+
+                @if ($role === 'chief')
+                <div
+                    class="grid max-w-screen-xl grid-cols-2 gap-8 p-4 mx-auto text-gray-900 sm:grid-cols-3 xl:grid-cols-4 dark:text-white sm:p-8">
+                    <div class="shadow shadow-gray-400/100 flex flex-col items-center justify-center rounded p-5">
+                        <div class="mb-2 text-5xl font-extrabold text-red-500">{{ $participantCount }}</div>
+                        <div class="text-gray-500 dark:text-gray-400">Asesi</div>
+                    </div>
+                    <div class="shadow shadow-gray-400/100 flex flex-col items-center justify-center rounded p-5">
+                        <div class="mb-2 text-5xl font-extrabold text-red-500">{{ $assessorCount }}</div>
+                        <div class="text-gray-500 dark:text-gray-400">Asesor</div>
+                    </div>
+                    <div class="shadow shadow-gray-400/100 flex flex-col items-center justify-center rounded p-5">
+                        <div class="mb-2 text-5xl font-extrabold text-red-500">{{ $scheduledParticipantCount }}</div>
+                        <div class="text-gray-500 dark:text-gray-400">Jadwal Uji Kompetensi</div>
+                    </div>
+                    <div class="shadow shadow-gray-400/100 flex flex-col items-center justify-center rounded p-5">
+                        <div class="mb-2 text-5xl font-extrabold text-red-500">{{ $certifiedParticipantCount }}</div>
+                        <div class="text-gray-500 dark:text-gray-400">Asesi Tersertifikasi</div>
+                    </div>
+                </div>
+                @endif
             </div>
         </div>
     </div>
